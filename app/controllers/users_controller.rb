d96c9777e3dcc526fa9bf985f6b09cd61b033087
betsy_manuel <betsy.manuel03@gmail.com>
2021-01-24 21:25:44 -0800

    fixed bug in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
             session[:user_id]=@user.id
             redirect_to root_path, notice: "Logged In!"
         else
-            render: new
+            render :new
         end
     end
 
